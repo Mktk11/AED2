e7c7c637ba6a6cb9cb66f0d17e4e70a5116040d1
Mktk11 <92392198+Mktk11@users.noreply.github.com>
2022-06-24 22:27:27 -0300

    Update Ec1.py

--- Ec1.py
+++ Ec1.py
@@ -1,3 +1,7 @@
+'''Questão 1: Implemente o algoritmo para calcular o n-ésimo elemento da sequência de Fibonacci visto em aula na linguagem de sua preferência (Python, C ou C++). 
+Execute o programa para n = 10, 20, 30, …, 100, 200, 300, ..., 1000. Apresente a implementação proposta e um relatório detalhando se os cálculos foram 
+corretamente realizados para todos os valores de n testados e quais dificuldades foram encontradas.'''
+
 def fib(n):
     if(n<=1):
         return 0
